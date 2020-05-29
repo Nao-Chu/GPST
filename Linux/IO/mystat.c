f3beb0beadec6697164117df517943df4f088ff4
Nao-Chu <1206278457@qq.com>
2020-05-29 14:56:00 +0800

    +mystat.c

---
+++ Linux/IO/mystat.c
@@ -0,0 +1,36 @@
+/********************************************************************
+    > File Name: mystat.c
+    > Author: zzp
+    > Created Time: 2020年05月29日 星期五 14时28分16秒
+ *******************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/stat.h>
+
+#define _GNU_SOURCE
+
+int main(int argc,char *argv[])
+{
+    struct stat sb;
+
+    //int ret = stat(argv[1],&sb);
+    int ret = lstat(argv[1],&sb);
+    if(ret == -1){
+	perror("stat eror");
+	exit(1);
+    }
+
+    if (S_ISREG(sb.st_mode)){
+	printf("It's a regular\n");
+    } else if (S_ISDIR(sb.st_mode)){
+	printf("It's a dir\n");
+    } else if (S_ISFIFO(sb.st_mode)){
+	printf("It's a pipe\n");
+    } else if (S_ISLNK(sb.st_mode)){
+	printf("It's a sym link\n");
+    }
+    return 0;
+}
